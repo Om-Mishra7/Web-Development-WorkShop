74cb419be201cd2a4fc6133bed34ac99d76701e8
Om Mishra <contact@om-mishra.com>
2024-03-13 15:21:39 +0530

    Updates

---
+++ Day-0/FLASK/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'About Us'
+
+@app.route('/greet/<username>')
+def greet(username):
+    return render_template('greet.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
